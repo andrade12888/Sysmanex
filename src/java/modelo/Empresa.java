2322965c5618e61a05f78de5c0a9a7360e5f1b51
SG0891660 <SG0891660@HN3UY89166001.Global.ad.sabre.com>
2017-05-29 20:47:05 -0300

    Clases modificadas: Archivo, Empresa. Modificacion en Empresa en la base de datos. Creacion de los TEST para archivo y Empresa

--- src/java/modelo/Empresa.java
+++ src/java/modelo/Empresa.java
@@ -5,6 +5,9 @@
  */
 package modelo;
 
+import accesoaDatos.Conecciones;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -16,6 +19,7 @@ public class Empresa {
    //<editor-fold defaultstate="collapsed" desc="Properties">
     private String rutEmpresa;    
     private ArrayList<Persona> listaPersonasAutorizadas;
+    private String nombreEmpresa;
     //</editor-fold>
    
    //<editor-fold defaultstate="collapsed" desc="Getters y Setters">
@@ -51,6 +55,20 @@ public class Empresa {
     public void setListaPersonasAutorizadas(ArrayList<Persona> listaPersonasAutorizadas) {
         this.listaPersonasAutorizadas = listaPersonasAutorizadas;
     }
+    
+    /**
+     * @return the nombreEmpresa
+     */
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    /**
+     * @param nombreEmpresa the nombreEmpresa to set
+     */
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
     //</editor-fold>    
 
    //<editor-fold defaultstate="collapsed" desc="Constructores">
@@ -58,8 +76,71 @@ public class Empresa {
         this.rutEmpresa = rutEmpresa;
         this.listaPersonasAutorizadas = listaPersonasAutorizadas;
     }
+
+    public Empresa(String rutEmpresa, ArrayList<Persona> listaPersonasAutorizadas, String nombreEmpresa) {
+        this.rutEmpresa = rutEmpresa;
+        this.listaPersonasAutorizadas = listaPersonasAutorizadas;
+        this.nombreEmpresa = nombreEmpresa;
+    }
  
+    
     //</editor-fold>
    
+    //POST: Retorna 1 si se ejecuta con exito, 0 si no reotorna nada, 2 si alguna de las variables
+    //      es nula (Rut o persona autorizada) o -1 si existe algun error en la secuancia SQL
+    protected int AgregarEmpresa() {
+        Conecciones conDB = new Conecciones();
+        int resultado;
+        if (!"".equals(this.rutEmpresa) || !(this.listaPersonasAutorizadas.isEmpty())) {
+            
+            for(Persona p:this.listaPersonasAutorizadas)
+            {
+                 String query = "INSERT INTO \"SysmanexSch1\".\"Empresa\"(\n"
+                    + "\"empresaRut\", \"empresaAutorizadosId\",\n"
+                    + "\"empresaNombre\")\n"
+                    + "   VALUES ('" + this.rutEmpresa + "', '" + p.getCiPersona() +"', '" + this.nombreEmpresa+"');";
+                 conDB.hacerConsultaIUD(query);                
+            }
+           resultado = 1;
+        } else {
+            resultado = 2;
+        }
+        return resultado;
+    }
+    
+     protected static ResultSet BuscarEmpresas() throws SQLException {
+        Conecciones conDB = new Conecciones();
+        ResultSet rs;
+
+        String query = "SELECT * FROM \"SysmanexSch1\".\"Empresa\" ORDER BY \"ArchivoNombre\" ASC;";
+        rs = conDB.hacerConsulta(query);
+
+        return rs;
+    }
+    
+     protected static ResultSet BuscarArchivoPorNombre(String nombre) throws SQLException {
+        Conecciones conDB = new Conecciones();
+        ResultSet rs;
+
+        String query = "SELECT * FROM \"SysmanexSch1\".\"Archivo\""
+                + " WHERE \"ArchivoNombre\" LIKE \'%" + nombre + "\'"
+                + " ORDER BY \"ArchivoNombre\" ASC;";
+        rs = conDB.hacerConsulta(query);
+
+        return rs;
+    }
+     
+     protected static ResultSet BuscarArchivoPorId(int idArchivo) throws SQLException {
+        Conecciones conDB = new Conecciones();
+        ResultSet rs;
+
+        String query = "SELECT * FROM \"SysmanexSch1\".\"Archivo\""
+                + " WHERE \"ArchivoId\" = "+idArchivo + ";";
+        rs = conDB.hacerConsulta(query);
+
+        return rs;
+    }
+
+    
 }
 
