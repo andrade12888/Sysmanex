f00dadb0e9e1c3a66d178a15b50900ec72e4949b
SG0891660 <SG0891660@HN3UY89166001.Global.ad.sabre.com>
2017-06-13 00:58:42 -0300

    Merge origin/master
    Conflicts:
    	src/java/modelo/Entidad.java

--- src/java/modelo/Persona.java
+++ src/java/modelo/Persona.java
@@ -8,6 +8,7 @@ package modelo;
 import accesoaDatos.Conecciones;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -85,39 +86,47 @@ public class Persona extends Entidad {
 
     //</editor-fold>
    
-
+//TODO: Ver refactor de los metods de Agregar PersonaEnUnidad y Empresa
     //Segun lo enviado desde el form, persona debe :
     //1: Agregar entidad(user)
     //2: Agregar persona con el id de Entiad que capturo del insert anterior
-    //3: Agregar en UnidadArmada o Empresa dicha persona 
+    //3: Agregar en UnidadArmada dicha persona 
     
     //TODO: Se puede devolver un numero diferente para cada transsaccion que falle
     //TODO: Como llegan los valores, por parametros o por objetos?
+    
+    //PRE: La unidad existe dado que viene del Form
     protected int AgregarPersonaEnUnidad(int unidadId) throws SQLException {
         Conecciones conDB = new Conecciones();
         int resultado =-1;
 
         //Controlo que los valores necesario para inrgesar una persona no sean vacios
-        if (!"".equals(this.ciPersona) && !"".equals(super.getNombreEntidad()) && !"".equals(this.apellidoPersona)&& !"".equals(super.getContrasenia()) ) 
+        if (!"".equals(this.ciPersona) &&
+                !"".equals(super.getNombreEntidad()) &&
+                !"".equals(this.apellidoPersona)&&
+                !"".equals(super.getContrasenia())&&
+                !"".equals(this.emailPersona) ) 
         {                     
             try {
                 //Seteo autocommit false para que todo se ejecute como una transaccion
                 conDB.getConnect().setAutoCommit(false);
 
                 //Agrego la entidad con la contrasena, username y rol que viene del form
-                Entidad e = new Entidad(super.getNombreEntidad(),super.getContrasenia(), super.getRol());
+                Entidad e = new Entidad(super.getNombreEntidad(),
+                        super.getContrasenia(),
+                        super.getRol());
                 //Me quedo con el id de entidad si se ejecuto correctamente
                 int entidadId = e.AgregarEntidad();
                 
                 //Si se agrego correctamente la entidad, agrego la persona con el id de entidad guardado
-                  String queryPersona = "INSERT INTO \"SysmanexSch1\".\"Persona\"(\n"
-                    + "\"personaCi\", \"personaNombre\", \"personaApellido\", \"personaEntidadId\")\n"
+                  String queryPersona = "INSERT INTO \"SysmanexSch1\".\"Persona\"(\"personaCi\", \"personaNombre\","
+                          + " \"personaApellido\", \"personaEntidadId\", \"personaEmail\")" 
                     + "   VALUES ('" + this.ciPersona + "', '" + this.nombrePersona + "', " +
-                     " '" + this.apellidoPersona + "', " + entidadId + ");";
+                     " '" + this.apellidoPersona + "', " + entidadId +"', " + this.emailPersona + ");";
                 conDB.hacerConsultaIUD(queryPersona);
                 
                 //Ahora agrego la persona a la unidad en la tabla UnidadTienePersona con el valor 
-                // de entidadId que viene del form
+                // de unidad que viene del form
                  String queryPersonaEnUnidad = "INSERT INTO \"SysmanexSch1\".\"UnidadTienePersona\"(\n"
                     + "\"unidadId\", \"personaCi\")\n"
                     + "   VALUES ('" + unidadId + "', " +this.ciPersona+");";
@@ -148,12 +157,90 @@ public class Persona extends Entidad {
                 }
             }
         } else {
-            System.err.print("Los campos en Nombre, Apellido y CI no pueden ser vacios.\n");
+            System.err.print("Los campos en Nombre, Apellido CI, Email no pueden ser vacios.\n");
+            resultado = 2;
+        }
+        return resultado;
+    }
+    
+    //Segun lo enviado desde el form, persona debe :
+    //1: Agregar entidad(user)
+    //2: Agregar persona con el id de Entiad que capturo del insert anterior
+    //3: Agregar en  Empresa dicha persona 
+    
+    //TODO: Se puede devolver un numero diferente para cada transsaccion que falle
+    //TODO: Como llegan los valores, por parametros o por objetos?
+    //PRE: La empresa existe dado que viene del Form
+    protected int AgregarPersonaEnEmpresa(String rutEmpresa) throws SQLException {
+        Conecciones conDB = new Conecciones();
+        int resultado =-1;
+
+        //Controlo que los valores necesario para inrgesar una persona no sean vacios
+        if (!"".equals(this.ciPersona) && !"".equals(super.getNombreEntidad()) && 
+                !"".equals(this.apellidoPersona)&& 
+                !"".equals(super.getContrasenia())&& 
+                !"".equals(this.emailPersona) ) 
+        {                     
+            try {
+                //Seteo autocommit false para que todo se ejecute como una transaccion
+                conDB.getConnect().setAutoCommit(false);           
+                //Me quedo con el id de entidad si se ejecuto correctamente
+                //Agrego la entidad con la contrasena, username y rol que viene del form
+                Entidad e = new Entidad(super.getNombreEntidad(),
+                        super.getContrasenia(),
+                        super.getRol());
+                int entidadId = e.AgregarEntidad();
+                
+                //Si se agrego correctamente la entidad, agrego la persona con el id de entidad guardado
+                  String queryPersona = "INSERT INTO \"SysmanexSch1\".\"Persona\"(\"personaCi\", \"personaNombre\","
+                          + " \"personaApellido\", \"personaEntidadId\", \"personaEmail\")" 
+                    + "   VALUES ('" + this.ciPersona + "', '" + this.nombrePersona + "', " +
+                     " '" + this.apellidoPersona + "', " + entidadId +"', " + this.emailPersona + ");";
+                conDB.hacerConsultaIUD(queryPersona);
+                
+//                ArrayList<Persona> listaAutorizados = new ArrayList<>();
+//                listaAutorizados.add(this);
+//                //Agrego la empresa con el nombre y rut que viene del Form
+//                Empresa emp = new Empresa(rutEmpresa,listaAutorizados, nombreEmpresa);
+                //Ahora agrego la persona a la unidad en la tabla UnidadTienePersona con el valor 
+                // de entidadId que viene del form
+                 String queryPersonaEnUnidad = "INSERT INTO \"SysmanexSch1\".\"EmpresaTienePersona\"(\n"
+                    + "\"empresaRut\", \"personaCi\")\n"
+                    + "   VALUES ('" + rutEmpresa + "', " +this.ciPersona+");";
+                conDB.hacerConsultaIUD(queryPersonaEnUnidad);
+                
+                conDB.getConnect().commit();
+                resultado = 1;
+
+            } catch (SQLException e) {
+
+                if (conDB.getConnect() != null) {
+                    try {
+                        System.err.print("Ocurrio un error ingresando la persona a la Empresa");
+                        conDB.getConnect().rollback();
+                        return -1;
+                    } catch (SQLException excep) {
+                        return -1;
+                    }
+                }
+
+            } finally {
+                if (conDB.getConnect() != null) {
+                    try {
+                        conDB.getConnect().setAutoCommit(true);
+                    } catch (SQLException ex) {
+                        throw ex;
+                    }
+                }
+            }
+        } else {
+            System.err.print("Los campos en Nombre, Apellido CI, Email no pueden ser vacios.\n");
             resultado = 2;
         }
         return resultado;
     }
 
+    //TODO: Definir que datos se tienen que modificar
     protected int ModificarPersona(String ciPersona) throws SQLException {
         Conecciones conDB = new Conecciones();
         int resultado;
